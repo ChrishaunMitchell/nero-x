8447b507161984c4c5f55f69fd751be162975fcb
ChrishaunMitchell <chris.critical172@gmail.com>
2023-02-23 02:35:37 -0600

    Template for Nero-X

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+return (
+	<div>
+	<h1>
+		Nero-X is a money exchange app that uses crypto
+		Go global with it
+	</h1>
+	</div>
+);
+};
+
+export default About;
