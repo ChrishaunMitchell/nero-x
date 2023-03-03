18d15951bb3dde48df5e5669fe6cc417544aeaa5
Chevyf <clfaceyy@gmail.com>
2023-03-02 23:06:53 -0500

    react navigation install

--- NeroX/tailwind.config.js
+++ NeroX/tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+  "./App.{js,ts,jsx,tsx}",
+  "./src/screens/**/*.{js,ts,jsx,tsx}",
+  "./src/components/**/*.{js,ts,jsx,tsx}",
+],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily:{
+
+        sans: ['"DM Sans"', ...defaultTheme.fontFamily.sans],
+      },
+
+      colors:{
+        sage: "#7F8C72", /** inside box 20 opacity */
+        white: "FFFFFF", 
+        dt: "232D19",/**lighter text opacity 80 */
+        
+
+      }
+    },
+
+
   },
   plugins: [],
 }
